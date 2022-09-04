875494aa988adecd3be3af5d7e8de57b778d333d
MHMasoon <mhmasoon2000@gmail.com>
2022-09-04 17:41:17 +0430

    add totalSupply function

--- masoon-coin.sol
+++ masoon-coin.sol
@@ -13,4 +13,8 @@ contract MasoonCoin {
     function decimals() pure public returns (uint8) {
         return 0;
     }
+
+    function totalSupply() pure public returns (uint256) {
+        return 1000000000000000000; // 10^18
+    }
 }
