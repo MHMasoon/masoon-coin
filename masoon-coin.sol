88e9e4f15bf6bce997b2706469c198ff7466aee1
MHMasoon <mhmasoon2000@gmail.com>
2022-09-07 10:34:19 +0430

    create allowance function

--- masoon-coin.sol
+++ masoon-coin.sol
@@ -60,4 +60,8 @@ contract MasoonCoin {
         allowance[sender][_spender] = _value;
         return true;
     }
+
+    function allowance(address _owner, address _spender) public view returns (uint256 remaining) {
+        return allowance[_owner][_spender];
+    }
 }
