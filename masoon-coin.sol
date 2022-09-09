4c926606173078b62583c3b6e1b0c010b0fbc633
MHMasoon <mhmasoon2000@gmail.com>
2022-09-09 09:14:05 +0430

    change totoalSupply + manual linting

--- masoon-coin.sol
+++ masoon-coin.sol
@@ -2,6 +2,19 @@
 pragma solidity 0.8.16;
 
 contract MasoonCoin {
+
+    constructor() {
+        address contractCreator = msg.sender;
+        accountBalance[contractCreator] += 1000;
+        emit Transfer(0x0000000000000000000000000000000000000000, contractCreator, 1000);
+    }
+
+    mapping(address => uint256) accountBalance;
+    mapping(address => mapping(address => uint256)) allowances;
+
+    event Transfer(address indexed _from, address indexed _to, uint256 _value);
+    event Approval(address indexed _owner, address indexed _spender, uint256 _value);
+
     function name() pure public returns (string memory) {
         return 'MasoonCoin';
     }
@@ -15,14 +28,8 @@ contract MasoonCoin {
     }
 
     function totalSupply() pure public returns (uint256) {
-        return 1000000000000000000; // 10^18
+        return 1000;
     }
-
-    event Transfer(address indexed _from, address indexed _to, uint256 _value);
-
-    event Approval(address indexed _owner, address indexed _spender, uint256 _value);
-
-    mapping(address => uint256) accountBalance;
 
     function balanceOf(address _owner) public view returns (uint256 balance) {
         return accountBalance[_owner];
@@ -34,8 +41,6 @@ contract MasoonCoin {
             accountBalance[sender] -= _value;
             accountBalance[_to] += _value;
             emit Transfer(sender, _to, _value);
-            // Here I should fire the Transfer event
-            // I should decide to allow 0 transfer or not
             return true;
         }
         else {
@@ -51,13 +56,10 @@ contract MasoonCoin {
             accountBalance[_to] += _value;
             emit Transfer(_from, _to, _value);
             return true;
-            // allow 0 transfer ?
         } else {
             // Throw
         }
     }
-
-    mapping(address => mapping(address => uint256)) allowances;
 
     // This function has a vulnerability (An Attack Vector on Approve/TransferFrom Methods)
     function approve(address _spender, uint256 _value) public returns (bool success) {
@@ -69,11 +71,5 @@ contract MasoonCoin {
 
     function allowance(address _owner, address _spender) public view returns (uint256 remaining) {
         return allowances[_owner][_spender];
-    }
-
-    constructor() {
-        address contractCreator = msg.sender;
-        accountBalance[contractCreator] += 1000000000000000000;
-        emit Transfer(0x0000000000000000000000000000000000000000, contractCreator, 1000000000000000000);
     }
 }
